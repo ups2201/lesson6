e7bcbb74c0a913e9bbe5ce2cefaf63dbbc410141
ups2201 <ups2201@gmail.com>
2024-04-12 18:48:55 +0300

    commit2

--- src/homework1/task3.js
+++ src/homework1/task3.js
@@ -1,5 +1,19 @@
-function sum(a, b) {
-    return a + b;
+/**
+ * 3.*Написать программу, которая запрашивает у
+ * пользователя ввод трёхзначного числа, а потом
+ * выводит в консоль сумму цифр введённого числа.
+ *
+ * @param x
+ * @returns {*}
+ */
+
+x = prompt('Input')
+
+function sumNumber(x) {
+    if (x.length != 3) {
+        return "Некоррктные входные данные"
+    }
+    return Number(x.charAt(0)) + Number(x.charAt(1)) + Number(x.charAt(2));
 }
 
-console.log(sum(1,2))
+console.log(sumNumber(x))
