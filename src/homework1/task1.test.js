a940e59481aaa8fdfa2b9528c629a6620b570139
ups2201 <ups2201@gmail.com>
2024-05-10 13:24:38 +0300

    step-3. Добавлен eslint и prettier

--- src/homework1/task1.test.js
+++ src/homework1/task1.test.js
@@ -1,7 +1,7 @@
-import { sum } from './task1';
+import { sum } from "./task1";
 
-describe('Sum', () => {
-    it('return sum of two numbers', function () {
-        expect(sum(1,2)).toEqual(3);
-    });
-})
+describe("Sum", () => {
+  it("return sum of two numbers", () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+});
